dcf8d63f16cb7dc87bf1eacc2d24e9c0debc1160
hanjiaming <47519540+guomaimang@users.noreply.github.com>
2024-04-09 00:39:46 +0800

    init project

---
+++ Hoptraf/src/main/java/tech/hirsun/hoptraf/HoptrafApplication.java
@@ -0,0 +1,13 @@
+package tech.hirsun.hoptraf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoptrafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoptrafApplication.class, args);
+    }
+
+}
